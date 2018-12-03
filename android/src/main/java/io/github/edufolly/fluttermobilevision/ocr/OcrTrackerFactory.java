8ca04f37bc661ee75525735f316b186b2781c9fd
raziAkbar <42175378+raziAkbar@users.noreply.github.com>
2018-12-03 08:49:59 +0500

    Update OcrTrackerFactory.java

--- android/src/main/java/io/github/edufolly/fluttermobilevision/ocr/OcrTrackerFactory.java
+++ android/src/main/java/io/github/edufolly/fluttermobilevision/ocr/OcrTrackerFactory.java
@@ -2,24 +2,31 @@ package io.github.edufolly.fluttermobilevision.ocr;
 
 import android.util.Log;
 
+import com.google.android.gms.common.util.Function;
 import com.google.android.gms.vision.MultiProcessor;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.text.TextBlock;
+
+import java.util.concurrent.Callable;
 
 import io.github.edufolly.fluttermobilevision.ui.GraphicOverlay;
 
 public class OcrTrackerFactory implements MultiProcessor.Factory<TextBlock> {
     private GraphicOverlay<OcrGraphic> graphicOverlay;
     private boolean showText;
+    public OcrCaptureActivity activity;
+    private String pattern;
 
-    public OcrTrackerFactory(GraphicOverlay<OcrGraphic> graphicOverlay, boolean showText) {
+    public OcrTrackerFactory(GraphicOverlay<OcrGraphic> graphicOverlay, boolean showText, OcrCaptureActivity activity, String pattern) {
         this.graphicOverlay = graphicOverlay;
         this.showText = showText;
+        this.activity = activity;
+        this.pattern = pattern;
     }
 
     @Override
     public Tracker<TextBlock> create(TextBlock textBlock) {
-        OcrGraphic graphic = new OcrGraphic(graphicOverlay, showText);
+        OcrGraphic graphic = new OcrGraphic(graphicOverlay, showText,this.activity, pattern);
         try {
             return new OcrGraphicTracker(graphicOverlay, graphic);
         } catch (Exception ex) {
