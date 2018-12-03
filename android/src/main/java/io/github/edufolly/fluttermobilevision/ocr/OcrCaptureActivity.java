a316c7c6fc81a5847da7b701863e1d50161ca6e8
raziAkbar <42175378+raziAkbar@users.noreply.github.com>
2018-12-03 08:49:00 +0500

    Update OcrCaptureActivity.java

--- android/src/main/java/io/github/edufolly/fluttermobilevision/ocr/OcrCaptureActivity.java
+++ android/src/main/java/io/github/edufolly/fluttermobilevision/ocr/OcrCaptureActivity.java
@@ -4,20 +4,48 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.RectF;
 import android.hardware.Camera;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.util.Function;
 import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 import io.github.edufolly.fluttermobilevision.ui.CameraSource;
 import io.github.edufolly.fluttermobilevision.util.AbstractCaptureActivity;
 import io.github.edufolly.fluttermobilevision.util.MobileVisionException;
 
 public final class OcrCaptureActivity extends AbstractCaptureActivity<OcrGraphic> {
+    public boolean next = true;
+    ArrayList<MyTextBlock> lists = new ArrayList<>();
+    public boolean textDetected(ArrayList<MyTextBlock> text) {
+        ArrayList<MyTextBlock> list = new ArrayList<>();
+        Log.d("razi","main");
+//        Log.d("razi",text.getValue());
+        list = text;
+//        list.add(new MyTextBlock(text));
+        if (!list.isEmpty()) {
+            Intent data = new Intent();
+            data.putExtra(OBJECT, list);
+            setResult(CommonStatusCodes.SUCCESS, data);
+            this.finish();
+            return true;
+        }
+
+        return false;
+    }
+
+
+
 
     @SuppressLint("InlinedApi")
     protected void createCameraSource() throws MobileVisionException {
@@ -25,8 +53,9 @@ public final class OcrCaptureActivity extends AbstractCaptureActivity<OcrGraphic
 
         TextRecognizer textRecognizer = new TextRecognizer.Builder(context)
                 .build();
-
-        OcrTrackerFactory ocrTrackerFactory = new OcrTrackerFactory(graphicOverlay, showText);
+        Log.d("razi",pattern);
+//        Function test = textDetected();
+        OcrTrackerFactory ocrTrackerFactory = new OcrTrackerFactory(graphicOverlay, showText, this, pattern);
 
         textRecognizer.setProcessor(
                 new MultiProcessor.Builder<>(ocrTrackerFactory).build());
@@ -43,6 +72,7 @@ public final class OcrCaptureActivity extends AbstractCaptureActivity<OcrGraphic
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
+        Log.d("razi","here");
         cameraSource = new CameraSource
                 .Builder(getApplicationContext(), textRecognizer)
                 .setFacing(camera)
@@ -51,7 +81,9 @@ public final class OcrCaptureActivity extends AbstractCaptureActivity<OcrGraphic
                 .setFlashMode(useFlash ? Camera.Parameters.FLASH_MODE_TORCH : null)
                 .setRequestedFps(fps)
                 .build();
+
     }
+
 
     protected boolean onTap(float rawX, float rawY) {
         ArrayList<MyTextBlock> list = new ArrayList<>();
